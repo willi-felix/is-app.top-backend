3ebc673c764b24fd207d2688b2261e3d8b9d63a5
ctih <nevalaonni@proton.me>
2024-03-09 17:46:03 +0200

    Logging?

--- funcs.py
+++ funcs.py
@@ -294,6 +294,7 @@ def modify_domain(domain: str, token: str, new_ip: str) -> tuple:
     data = get_data(username=username)
     if password_is_correct(username=username,password=password): # correct creds
       domains: dict = data["domains"]
+      print(domains)
       if(domain in domains): # if the doman exists
         fernet = Fernet(bytes(os.getenv('ENC_KEY'), 'utf-8'))
         data_ = {
