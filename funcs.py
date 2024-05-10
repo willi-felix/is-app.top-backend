880268d91667f8b28c2d63ecf3c3dd32cce6dca2
ctih <nevalaonni@proton.me>
2024-05-10 20:46:39 +0300

    added new status

--- funcs.py
+++ funcs.py
@@ -319,6 +319,7 @@ def modify_domain(domain: str, token: str, new_ip: str, type_:str) -> tuple:
         if(response.status_code==200):
           add_domain_to_user(user=username,domain=domain,ip=new_ip,domain_id=None,type=type_)
           return "OK",200 # if its ok, then its ok!
+        return "Interal Server Error", 500
       else:
         return f'No', 403 # if user does not own the domain
     else: 
