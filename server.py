69d9679cebc600ae4fc433c5e0c7ca07b1fb4e05
ctih <nevalaonni@proton.me>
2024-05-13 18:32:41 +0300

    fixed 500

--- server.py
+++ server.py
@@ -113,7 +113,7 @@ def del_domain():
 @app.route("/delete-user",methods=["POST"])
 def del_user():
   token = request.json.get("TOKEN")
-  return delete_user()
+  return delete_user(token)
 
 if(__name__=="__main__"):
   app.run(host='0.0.0.0', port=5000)
