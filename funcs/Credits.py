91cadfd9eff68ce895873b85aab41ca590636e10
ctih <78687256+ctih1@users.noreply.github.com>
2024-08-12 21:31:48 +0300

    disabled cache on credit convertion

--- funcs/Credits.py
+++ funcs/Credits.py
@@ -16,6 +16,7 @@ class Credits:
         if(credits<DOMAIN_PRICE): raise AttributeError("Not enough credits")
         self.db.update_data(token.username,"credits",credits-DOMAIN_PRICE)
         self.db.update_data(token.username,"permissions.max-domains",max_domains+1)
+        self.db.remove_from_cache(token)
         return True
     
     def get(self,token:Token) -> int:
