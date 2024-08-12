00b8ddb3bc9230f584e7559c44dc4aec6ea45ea2
ctih <78687256+ctih1@users.noreply.github.com>
2024-08-12 22:17:13 +0300

    fixed invalid key

--- funcs/Credits.py
+++ funcs/Credits.py
@@ -12,7 +12,7 @@ class Credits:
         __data=self.db.get_data(token)
         credits = __data.get("credits",0)
         max_domains = __data["permissions"].get("max-domains",4)
-        if(not __data.get("api-keys",{}).get("credits",False)): raise PermissionError("Not a beta tester")
+        if(not __data.get("feature-flags",{}).get("credits",False)): raise PermissionError("Not a beta tester")
         if(credits<DOMAIN_PRICE): raise AttributeError("Not enough credits")
         self.db.update_data(token.username,"credits",credits-DOMAIN_PRICE)
         self.db.update_data(token.username,"permissions.max-domains",max_domains+1)
@@ -21,5 +21,5 @@ class Credits:
     
     def get(self,token:Token) -> int:
         __data=self.db.get_data(token)
-        if(not __data.get("api-keys",{}).get("credits",False)): raise PermissionError("Not a beta tester")
+        if(not __data.get("feature-flags",{}).get("credits",False)): raise PermissionError("Not a beta tester")
         return __data.get("credits",0)
