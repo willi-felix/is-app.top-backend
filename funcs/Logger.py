de478f04679d95d6da1d121740445eefa7414fba
ctih <78687256+ctih1@users.noreply.github.com>
2024-08-15 17:42:08 +0300

    force pushing to master

--- funcs/Logger.py
+++ funcs/Logger.py
@@ -21,7 +21,7 @@ class Logger:
         return values.get(importance,default)
         
     async def __send_to_webhook(self,importance:str,message:str) -> None:
-        return
+        return None
         try:
             if(importance in ["warning","permission","error","critical"]):
                 url = self.webhook
