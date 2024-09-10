42821cf7ba13c471df5792a5ca28638b2a2d95e1
ctih1 <contact@frii.site>
2024-09-10 17:37:43 +0300

    fixed 404 not being returned on no domains

--- connector.py
+++ connector.py
@@ -152,6 +152,7 @@ def get_domains(token:str) -> Response:
     status:dict = domain.get_user_domains(database,Token(token))
     if("Error" in status):
         return Response(status=responses.get(status["code"]))
+    if(len(status) == 0): return Response(status=404)
     return Response(response=json.dumps(status),status=200,mimetype="application/json")
 
 #/is-verified
